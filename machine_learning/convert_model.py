9dc17a121081dc5133feedfdb468ed4c30c3034a
Alexander Glenn <43507162+alexg143@users.noreply.github.com>
2021-04-27 14:29:29 -0700

    Update convert_model.py

--- machine_learning/convert_model.py
+++ machine_learning/convert_model.py
@@ -4,10 +4,12 @@ Created on Tue Apr 27 13:59:08 2021
 
 * I did this in google colab, therefore you will need to change 
  the directories of files before running
+ 
+* Put each code block in a seperate block in colab (seperated by ---)
 
 @author: Alex (alexander.glenn@wsu.edu)
 """
-
+#------------------------------------------------------------------------
 !pip install tensorflow==2.1.0
 
 import tensorflow as tf
@@ -15,8 +17,7 @@ from tensorflow import keras
 from google.colab import drive
 import os
 drive.mount('/content/drive')
-
-
+#------------------------------------------------------------------------
 os.chdir('/content/drive/My Drive/Colab Notebooks/model_testing')
 
 model = keras.models.load_model("potato_model_4_11_21.h5", compile=True)
@@ -28,3 +29,4 @@ tflite_model = converter.convert()
 # Save the model.
 with open('potato_model_4_11_21.tflite', 'wb') as f:
   f.write(tflite_model)
+#------------------------------------------------------------------------
